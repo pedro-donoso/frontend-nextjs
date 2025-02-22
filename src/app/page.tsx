06bce41611ecd63707a0187d7d8413904dd3a130
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-22 13:10:44 -0300

    formato

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,7 @@ const Home = () => {
     <div className="min-h-screen bg-black">
       <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
         <div className="px-4 py-6 sm:px-0">
-          <h1 className="text-3xl font-bold text-white mb-8">
-            Lista de Usuarios
-          </h1>
+          <h1 className="text-3xl font-bold text-white mb-8">Lista de Usuarios</h1>
           <ListaUsuario />
         </div>
       </div>
