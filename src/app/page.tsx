86922b11d5b7d64ce54322ac699df061f8b02fd9
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-24 01:17:32 -0300

    pasos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,7 @@ const Home = () => {
     <div className="min-h-screen bg-black">
       <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
         <div className="px-4 py-6 sm:px-0">
-          // Titulo de la pagina
           <h1 className="text-3xl font-bold text-white mb-8">Lista de Usuarios</h1>
-          // Componente ListaUsuario
           <ListaUsuario />
         </div>
       </div>
